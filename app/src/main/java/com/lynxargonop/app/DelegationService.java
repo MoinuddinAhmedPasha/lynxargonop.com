2a6a1b4e6348f4909cdf7de68ddfcea0c7ba04a6
Moinuddin Ahmed Pasha <faisalwings69@gmail.com>
2026-07-16 16:30:55 +0530

    Initial Django setup

---
+++ app/src/main/java/com/lynxargonop/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.lynxargonop.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
---
+++ public/ads.js
@@ -0,0 +1,5 @@
+(function(s){
+  s.dataset.zone = "10840637"; // your Monetag zone ID
+  s.src = "https://nap5k.com/tag.min.js";
+  (document.head || document.documentElement).appendChild(s);
+})(document.createElement('script'));
